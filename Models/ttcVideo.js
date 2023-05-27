f846426275e48d67da32d2b3fe25b2247faea376
syed abdullah ali <87133797+syedabdullahali@users.noreply.github.com>
2023-05-27 13:04:31 +0530

    Update ttcVideo.js

--- Models/ttcVideo.js
+++ Models/ttcVideo.js
@@ -2,12 +2,10 @@ let mongoose = require('mongoose')
 
 
 let ttcVideo = new mongoose.Schema({
-    srNo:String,
-    courseName:String,
-    formalOfVideos:String,
-    sessionNo:String,
-    videos:String,
-    driveLink:String,	
+      courseName:String,
+      formalOfVideos:String,
+      sessionNo:String,
+      videoLink:String,
 }, { timestamps: true })
 
 module.exports =   mongoose.model('ttcVideo', ttcVideo);
