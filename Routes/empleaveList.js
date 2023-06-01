39092788491fbb85823069c82938f7d8eaa8c4fa
syedabdullahali <syedabdullah380@gmail.com>
2023-06-01 20:16:28 +0530

    new product added

--- Routes/empleaveList.js
+++ Routes/empleaveList.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 //modelName
 const empleaveList = require('../Models/empleaveList')
+const leaveSetUpMaster = require('../Models/leaveSetUpMaster')
+const employee = require('../Models/employeeForm')
 
 
 router.get('/all', async function (req, res) {
@@ -13,12 +15,63 @@ router.get('/all', async function (req, res) {
     }
 })
 
+
+
+router.get('/emp-leave-info', async function (req, res) {
+    try {
+        const response1 = await employee.find({selected:'Select'})
+        const response2 = await leaveSetUpMaster.find()
+        const response3 = await empleaveList.find()
+
+
+        
+        const empData = response1.map((el)=>{
+            return  {
+             empName:el.FullName,
+             memBerId:el._id,
+             empId:el.EmployeeID,
+             leaveDetails:{
+                ...response2.map((el2)=>{
+                    let details= [] 
+                    let useleave=0 
+
+                    response3.forEach(el3 => {
+                        if(el2.year===el3.year&&el3.MemberId===el._id.toString()){
+                             details.push(el3)
+                             useleave+=+el3.useLeave
+                        }    
+                })
+
+                    return {
+                        totalLeave:el2.totalLeave,
+                        noOfSl:+el2.noOfSl,
+                        noOfCl:+el2.noOfCl,
+                        noOfPl:+el2.noOfPl,
+                        year:+el2.year,
+                        useleave,
+                        leaveReport:details,     
+                    }
+                })}
+             }
+           })
+
+        //    console.log(empData)
+
+        return res.status(200).json(empData);
+    } catch (err) {
+        return res.status(500).json({ error: err })
+    }
+})
+
+
+
+
 router.get('/emp/:id', async function (req, res) {
 
     const {id} = req.params
 
     try {
-        const response = await empleaveList.find({employeeID:id})
+        const response = await empleaveList.find({empId:id})
         return res.status(200).json(response);
     } catch (err) {
         return res.status(500).json({ error: err })
