e2738fd3fe8293b20793c8dc7393b08dc1ada25c
syed abdullah ali <87133797+syedabdullahali@users.noreply.github.com>
2023-06-02 18:15:29 +0530

    Update clientSupport.js

--- CRM/ClientManagement/clientSupport.js
+++ CRM/ClientManagement/clientSupport.js
@@ -4,39 +4,30 @@ const clientSupportSchema = mongoose.Schema(
     {
         Sr_No: {
             type: String,
-            required: true,
         },
         Client_Name: {
             type: String,
-            required: true,
         },
         Regular_Mobile_No: {
             type: String,
-            required: true,
         },
         Type_Of_Request: {
             type: String,
-            required: true,
         },
         Request_Date: {
             type: String,
-            required: true
         },
         Request_Details: {
             type: String,
-            required: true
         },
         Status: {
             type: Boolean,
-            required: true
         },
         Medium: {
             type: String,
-            required: true
         },
         Delete: {
             type: String,
-            required: true
         },
         
     },
