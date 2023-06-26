f7c987a991bf14e77d003f5a81d7174d3e85780e
syedabdullahali <syedabdullah380@gmail.com>
2023-06-26 12:13:32 +0530

    added berrear token in employeeForm api

--- index.js
+++ index.js
@@ -196,6 +196,7 @@ app.use('/ttcPDFDetails',userValidate,require('./Routes/ttcPDFDetails'))
 app.use('/empleaveList',userValidate,require('./Routes/empleaveList'))
 app.use('/allRight',userValidate,require('./AllRight/allRightRoute'))
 app.use('/center-partner',userValidate,require('./Master/CenterPartner/centerPartnerRoute'))
+app.use('/employeeForm',userValidate,require('./Routes/employeeForm'))
 
 
 
