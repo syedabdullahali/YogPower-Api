88c55701a689f45ac821d3faa4d4e7b8553cc143
syedabdullahali <syedabdullah380@gmail.com>
2023-06-30 16:03:24 +0530

    added new route to stock Order List

--- Inventory/StockOrderList/stockOrderList.js
+++ Inventory/StockOrderList/stockOrderList.js
@@ -71,6 +71,13 @@ const StockOrderListSchema = mongoose.Schema(
         sellerId:{
          type: String
       },
+      empNameC:String,
+      employeeIDC:String,
+      employeeMongoId: mongoose.Schema.Types.ObjectId,
+      partnerAdminMongoId: mongoose.Schema.Types.ObjectId,
+      centerNameC:String,
+      centerCodeC:String,
+      adminNameC:String,
 
     },
 )
