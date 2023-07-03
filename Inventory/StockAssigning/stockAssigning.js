178fbd8d8deef24edadf672a57dc1460b624651a
syedabdullahali <syedabdullah380@gmail.com>
2023-07-03 13:50:39 +0530

    added validation route in

--- Inventory/StockAssigning/stockAssigning.js
+++ Inventory/StockAssigning/stockAssigning.js
@@ -53,6 +53,13 @@ const StockAssigningSchema = mongoose.Schema(
         Total_Price:{
               type: String,
         },
+        empNameC:String,
+        employeeIDC:String,
+        employeeMongoId: mongoose.Schema.Types.ObjectId,
+        partnerAdminMongoId: mongoose.Schema.Types.ObjectId,
+        centerNameC:String,
+        centerCodeC:String,
+        adminNameC:String,
     },
 )
 
