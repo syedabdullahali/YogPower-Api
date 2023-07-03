de83f769b9b723d9043aea4d44f9873c6bb82981
syedabdullahali <syedabdullah380@gmail.com>
2023-07-03 13:45:38 +0530

    added validation route in

--- Inventory/IMPCallList/allSupplierList.js
+++ Inventory/IMPCallList/allSupplierList.js
@@ -30,6 +30,13 @@ const AllSupplierListSchema = mongoose.Schema(
             type: String,
             required: true
         },
+        empNameC:String,
+        employeeIDC:String,
+        employeeMongoId: mongoose.Schema.Types.ObjectId,
+        partnerAdminMongoId: mongoose.Schema.Types.ObjectId,
+        centerNameC:String,
+        centerCodeC:String,
+        adminNameC:String,
         
     },
 )
