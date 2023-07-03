ffc099b6af95890e042ffa94fa1998bf4d0408b8
syedabdullahali <syedabdullah380@gmail.com>
2023-07-03 15:25:32 +0530

    new Route

--- Inventory/IMPCallList/guestList.js
+++ Inventory/IMPCallList/guestList.js
@@ -2,34 +2,13 @@ const mongoose = require('mongoose')
 
 const GuestListSchema = mongoose.Schema(
     {
-        Sr_No: {
-            type: String,
-            required: true,
-        },
-        Name: {
-            type: String,
-            required: true,
-        },
-        Mobile: {
-            type: String,
-            required: true
-        },
-        Email_Id: {
-            type: String,
-            required: true
-        },
-        Address: {
-            type: String,
-            required: true
-        },
-        Category: {
-            type: String,
-            required: true
-        },
-        Event: {
-            type: String,
-            required: true
-        },
+        username: String,
+        name: String,
+        mobile: String,
+        email: String, 
+        category: String, 
+        address: String, 
+        company: String,
         empNameC:String,
         employeeIDC:String,
         employeeMongoId: mongoose.Schema.Types.ObjectId,
