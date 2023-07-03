6d0618eba91cd9dd1410fd0608f2baa044d8b875
syedabdullahali <syedabdullah380@gmail.com>
2023-07-03 14:34:49 +0530

    added new

--- Inventory/IMPCallList/impCallList.js
+++ Inventory/IMPCallList/impCallList.js
@@ -2,36 +2,21 @@ const mongoose = require('mongoose')
 
 const impCallList = mongoose.Schema(
     {
-        Sr_No: {
-            type: String,
-        },
-        Name: {
-            type: String,
-        },
-        Mobile: {
-            type: String
-        },
-        Email_Id: {
-            type: String
-        },
-        Address: {
-            type: String
-        },
-        Category: {
-            type: String
-        },
-        Company_Name: {
-            type: String
-        },
+        username: String,
+        name: String,
+        mobile: String,
+        email: String, 
+        category: String, 
+        address: String, 
+        company: String,
         empNameC:String,
         employeeIDC:String,
         employeeMongoId: mongoose.Schema.Types.ObjectId,
         partnerAdminMongoId: mongoose.Schema.Types.ObjectId,
         centerNameC:String,
         centerCodeC:String,
-        adminNameC:String,
-        
-    },
+        adminNameC:String
+    }
 )
 
 
