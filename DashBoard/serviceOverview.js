4a49c4795bba52a966ccadbde79f398eebcedb66
syedabdullahali <syedabdullah380@gmail.com>
2023-07-24 18:24:34 +0530

    adding new

--- DashBoard/serviceOverview.js
+++ DashBoard/serviceOverview.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const batchModule =  require('../Models/Batch')
-const invoice =  require('../Models/Invoice')
+const invoiceModule =  require('../Models/Invoice')
 
 
 
@@ -32,69 +32,69 @@ function toHandleServiceOverview(serviceData,invoiceData){
 
  //+'-active'
 //+'-inactive'
- const map = new Map()
-    for (const i in  serviceData){
-    const service = serviceData[i].service_variation.trim().toLocaleLowerCase()   
+//  const map = new Map()
+//     for (const i in  serviceData){
+//     const service = serviceData[i].service_variation.trim().toLocaleLowerCase()   
 
-    const initialObj = serviceData[i].typeOfTrainer=== 'In door'?{out:0,in:1,name:service}:{out:1,in:0,name:service}
+//     const initialObj = serviceData[i].typeOfTrainer=== 'In door'?{out:0,in:1,name:service}:{out:1,in:0,name:service}
 
 
-    if(!map.has(service+'-active') && serviceData[i].status ){
-       map.set(service+'-active',initialObj)
-     }else if(map.has(service+'-active') &&  serviceData[i].status){
-        map.set(service+'-active',{out:map.get(service+'-active').out+initialObj.out,in:map.get(service+'-active').in+initialObj.in,name:service})
-      }
-    if(!map.has(service+'-inactive') && !serviceData[i].status ){
-        map.set(service+'-inactive',initialObj)
-    }else if(map.has(service+'-inactive') &&  !serviceData[i].status){
-        map.set(service+'-inactive',{out:map.get(service+'-inactive').out+initialObj.out,in:map.get(service+'-inactive').in+initialObj.in,name:service})
+//     if(!map.has(service+'-active') && serviceData[i].status ){
+//        map.set(service+'-active',initialObj)
+//      }else if(map.has(service+'-active') &&  serviceData[i].status){
+//         map.set(service+'-active',{out:map.get(service+'-active').out+initialObj.out,in:map.get(service+'-active').in+initialObj.in,name:service})
+//       }
+//     if(!map.has(service+'-inactive') && !serviceData[i].status ){
+//         map.set(service+'-inactive',initialObj)
+//     }else if(map.has(service+'-inactive') &&  !serviceData[i].status){
+//         map.set(service+'-inactive',{out:map.get(service+'-inactive').out+initialObj.out,in:map.get(service+'-inactive').in+initialObj.in,name:service})
 
-       }
-     }
+//        }
+//      }
 
-     for(let [key,values] of map.entries()){
+//      for(let [key,values] of map.entries()){
        
         
-        const obj = {out:values.out,in:values.in,service:values.name}
+//         const obj = {out:values.out,in:values.in,service:values.name}
 
-        if(key.split('-').at(-1)==='active'){
-         serviceOverViewObj.service.activeServiceNum+=(values.in+values.out)
-         serviceOverViewObj.service.activeServiceInDoorNum+=(values.in)
-         serviceOverViewObj.service.activeServiceOutDoorNum+=(values.out)
+//         if(key.split('-').at(-1)==='active'){
+//          serviceOverViewObj.service.activeServiceNum+=(values.in+values.out)
+//          serviceOverViewObj.service.activeServiceInDoorNum+=(values.in)
+//          serviceOverViewObj.service.activeServiceOutDoorNum+=(values.out)
 
 
-         serviceOverViewObj.service.activeService.push( obj )
-        }
-        if(key.split('-').at(-1)==='inactive'){
-            serviceOverViewObj.service.inactiveServiceNum+=(values.in+values.out)
-            serviceOverViewObj.service.inactiveServiceInDoorNum+=(values.in)
-            serviceOverViewObj.service.inactiveServiceOutDoorNum+=(values.out)
+//          serviceOverViewObj.service.activeService.push( obj )
+//         }
+//         if(key.split('-').at(-1)==='inactive'){
+//             serviceOverViewObj.service.inactiveServiceNum+=(values.in+values.out)
+//             serviceOverViewObj.service.inactiveServiceInDoorNum+=(values.in)
+//             serviceOverViewObj.service.inactiveServiceOutDoorNum+=(values.out)
 
-            serviceOverViewObj.service.inactiveService.push(obj)
-           }
-    }
+//             serviceOverViewObj.service.inactiveService.push(obj)
+//            }
+//     }
 
-   const map2 = new Map()
+//    const map2 = new Map()
 
-    for (const i in invoiceData){
-        const serviceName= invoiceData[i].ServiceName.trim().toLocaleLowerCase()   
-        const collectedAmount = invoiceData[i].Receipts.reduce((crr,el)=>crr+ (+el.PaidAmount),0)  + invoiceData[i].paidAmount 
+//     for (const i in invoiceData){
+//         const serviceName= invoiceData[i].ServiceName.trim().toLocaleLowerCase()   
+//         const collectedAmount = invoiceData[i].Receipts.reduce((crr,el)=>crr+ (+el.PaidAmount),0)  + invoiceData[i].paidAmount 
 
-        if(!map2.has(serviceName)){
-           map2.set(serviceName,{amount:invoiceData[i].amount,colectedAmount: collectedAmount})
-        }else if(map2.has(serviceName)){
-            const amount2 = map2.get(serviceName).amount
-            const collectedAmount2 = map2.get(serviceName).colectedAmount
-            map2.set(serviceName,{amount:amount2+invoiceData[i].amount,colectedAmount: collectedAmount2+collectedAmount})
-        }
+//         if(!map2.has(serviceName)){
+//            map2.set(serviceName,{amount:invoiceData[i].amount,colectedAmount: collectedAmount})
+//         }else if(map2.has(serviceName)){
+//             const amount2 = map2.get(serviceName).amount
+//             const collectedAmount2 = map2.get(serviceName).colectedAmount
+//             map2.set(serviceName,{amount:amount2+invoiceData[i].amount,colectedAmount: collectedAmount2+collectedAmount})
+//         }
 
-    }
+//     }
  
 
-    for(let [key,values] of map2.entries()){     
-        const obj = {...values,serviceName:key}
-        serviceOverViewObj.serviceIncome.allServiceIncomeData.push(obj)
-    }
+//     for(let [key,values] of map2.entries()){     
+//         const obj = {...values,serviceName:key}
+//         serviceOverViewObj.serviceIncome.allServiceIncomeData.push(obj)
+//     }
 
 
     return serviceOverViewObj 
@@ -105,11 +105,12 @@ router.get('/all', async function (req, res) {
 
     try {
         const response =  batchModule.find()
-        const response1 = invoice.find()
+        const response1 = invoiceModule.find()
         const allData = await Promise.all([response,response1])
 
         return res.status(200).json(toHandleServiceOverview(allData[0],allData[1]))
     } catch (err) {
+        console.log(err)
         return res.status(500).json({ error: err })
     }
 })
@@ -120,7 +121,7 @@ router.get('/filter-by-employee/:employeeId', async function (req, res) {
 
     try {
         const response =  batchModule.find({employeeMongoId: employeeId})
-        const response1 = invoice.find({employeeMongoId: employeeId})
+        const response1 = invoiceModule.find({employeeMongoId: employeeId})
         const allData = await Promise.all([response,response1])
 
         return res.status(200).json(toHandleServiceOverview(allData[0],allData[1]))
