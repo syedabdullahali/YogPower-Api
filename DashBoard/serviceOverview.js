bc3e148cc4133a9016c49635324ff002d9d216ce
syedabdullahali <syedabdullah380@gmail.com>
2023-07-24 18:46:15 +0530

    adding new

--- DashBoard/serviceOverview.js
+++ DashBoard/serviceOverview.js
@@ -52,27 +52,27 @@ function toHandleServiceOverview(serviceData,invoiceData){
        }
      }
 
-//      for(let [key,values] of map.entries()){
+     for(let [key,values] of map.entries()){
        
         
-//         const obj = {out:values.out,in:values.in,service:values.name}
+        const obj = {out:values.out,in:values.in,service:values.name}
 
-//         if(key.split('-').at(-1)==='active'){
-//          serviceOverViewObj.service.activeServiceNum+=(values.in+values.out)
-//          serviceOverViewObj.service.activeServiceInDoorNum+=(values.in)
-//          serviceOverViewObj.service.activeServiceOutDoorNum+=(values.out)
+        if(key.split('-').at(-1)==='active'){
+         serviceOverViewObj.service.activeServiceNum+=(values.in+values.out)
+         serviceOverViewObj.service.activeServiceInDoorNum+=(values.in)
+         serviceOverViewObj.service.activeServiceOutDoorNum+=(values.out)
 
 
-//          serviceOverViewObj.service.activeService.push( obj )
-//         }
-//         if(key.split('-').at(-1)==='inactive'){
-//             serviceOverViewObj.service.inactiveServiceNum+=(values.in+values.out)
-//             serviceOverViewObj.service.inactiveServiceInDoorNum+=(values.in)
-//             serviceOverViewObj.service.inactiveServiceOutDoorNum+=(values.out)
+         serviceOverViewObj.service.activeService.push( obj )
+        }
+        if(key.split('-').at(-1)==='inactive'){
+            serviceOverViewObj.service.inactiveServiceNum+=(values.in+values.out)
+            serviceOverViewObj.service.inactiveServiceInDoorNum+=(values.in)
+            serviceOverViewObj.service.inactiveServiceOutDoorNum+=(values.out)
 
-//             serviceOverViewObj.service.inactiveService.push(obj)
-//            }
-//     }
+            serviceOverViewObj.service.inactiveService.push(obj)
+           }
+    }
 
    const map2 = new Map()
 
